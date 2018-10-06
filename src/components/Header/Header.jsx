98004fcad6348130bc36570b9fa77a263a744a02
juanvitofer <j.v.torres.fernandez@gmail.com>
2018-10-06 18:12:23 +0200

    Components and Props

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ class Header extends React.Component {
             <div className="Header">
                 <div className="Header__logo">
                     <img alt="Accenture Logo"
-                    src="https://www.accenture.com/t20180820T081710Z__w__/us-en/_acnmedia/Accenture/Dev/Redesign/Acc_Logo_Black_Purple_RGB.PNG"
+                    src={this.props.logo}
                     />
                 </div>
                 <h1 className="Header__title">II OPENATHON Custom Open Cloud</h1>
--- src/controllers/App/App.jsx
+++ src/controllers/App/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header logo="https://www.accenture.com/t20180820T081710Z__w__/us-en/_acnmedia/Accenture/Dev/Redesign/Acc_Logo_Black_Purple_RGB.PNG" />
         <p className="Main">
           Main content.
         </p>
